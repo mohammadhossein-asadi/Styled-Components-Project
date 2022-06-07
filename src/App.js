24f76defcd32031eb6b53d47671fe6574b1577e0
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-06-07 19:18:36 +0430

    create footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Card from "./components/Card";
 import { Container } from "./components/styles/Container.styled";
 import GlobalStyles from "./components/styles/Global";
@@ -25,6 +26,7 @@ function App() {
             <Card key={index} item={item} />
           ))}
         </Container>
+        <Footer />
       </>
     </ThemeProvider>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,37 @@
 import { Container } from "./styles/Container.styled";
 import { Flex } from "./styles/Flex.styled";
+import { StyledFooter } from "./styles/Footer.styled";
 
-function Footer() {
+export default function Footer() {
   return (
-    <div>
+    <StyledFooter>
       <Container>
-        <img src="./logo_white.svg" alt="" />
+        <img src="./images/logo_white.svg" alt="" />
+
+        <Flex>
+          <ul>
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua
+            </li>
+            <li>+1-543-123-4567</li>
+            <li>example@huddle.com</li>
+          </ul>
+          <ul>
+            <li>About Us</li>
+            <li>What We Do</li>
+            <li>FAQ</li>
+          </ul>
+
+          <ul>
+            <li>Career</li>
+            <li>Blog</li>
+            <li>Contact Us</li>
+          </ul>
+        </Flex>
+
+        <p>&copy; 2021 Huddle. All rights reserved</p>
       </Container>
-    </div>
+    </StyledFooter>
   );
 }
-
-export default Footer;
