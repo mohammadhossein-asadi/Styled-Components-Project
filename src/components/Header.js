53b297637c9d11696b1e3002472de49ab10e8f3e
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-06-07 11:16:55 +0430

    added new file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
-import { StyledHeader } from "./styles/Header.styled";
+import { Logo, Nav, StyledHeader } from "./styles/Header.styled";
+import { Container } from "./styles/Container.styled";
+import { Button } from "./styles/Button.styled";
 
 function Header() {
   return (
     <StyledHeader>
-      <h1>Hubble</h1>
+      <Container>
+        <Nav>
+          <Logo src="./images/logo.svg" alt="" />
+          <Button>Try It Free</Button>
+        </Nav>
+      </Container>
     </StyledHeader>
   );
 }
