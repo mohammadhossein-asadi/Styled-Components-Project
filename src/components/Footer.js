48bb9959d6170060330db49c29d4caef6e659a34
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-06-07 17:42:31 +0430

    create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import { Container } from "./styles/Container.styled";
+import { Flex } from "./styles/Flex.styled";
+
+function Footer() {
+  return (
+    <div>
+      <Container>
+        <img src="./logo_white.svg" alt="" />
+      </Container>
+    </div>
+  );
+}
+
+export default Footer;
