42b42fc88745bb16ceee88d7b48393a8e772e1cc
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-06-07 11:32:35 +0430

    create new files and added media query for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
-import { Logo, Nav, StyledHeader } from "./styles/Header.styled";
+import { Image, Logo, Nav, StyledHeader } from "./styles/Header.styled";
 import { Container } from "./styles/Container.styled";
+import { Flex } from "./styles/Flex.styled";
 import { Button } from "./styles/Button.styled";
 
 function Header() {
@@ -10,6 +11,24 @@ function Header() {
           <Logo src="./images/logo.svg" alt="" />
           <Button>Try It Free</Button>
         </Nav>
+
+        <Flex>
+          <div>
+            <h1>Build The Community Your Fans Will Love</h1>
+
+            <p>
+              Huddle re-imagines the way we build communities. You have a voice,
+              but so does your audience. Create connections with your users as
+              you engage in genuine discussion.
+            </p>
+
+            <Button bg="#ff0099" color="#fff">
+              Get Started For Free
+            </Button>
+          </div>
+
+          <Image src="./images/illustration-mockups.svg" alt="" />
+        </Flex>
       </Container>
     </StyledHeader>
   );
