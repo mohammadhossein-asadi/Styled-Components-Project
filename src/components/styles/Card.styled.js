5f62805b0cc625120acb7e2d976db36decc2d5f3
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-06-07 11:55:20 +0430

    change flex direction value and added props

--- src/components/styles/Card.styled.js
+++ src/components/styles/Card.styled.js
@@ -8,7 +8,7 @@ export const StyledCard = styled.div`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
   margin: 40px 0;
   padding: 60px;
-  flex-direction: row-reverse;
+  flex-direction: ${({ layout }) => layout || "row"};
 
   img {
     width: 80%;
